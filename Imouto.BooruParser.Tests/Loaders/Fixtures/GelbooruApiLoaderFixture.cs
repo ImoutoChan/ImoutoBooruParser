44bf592f3c2d9666595daba1bcca5ec86b547a7c
ImoutoChan <kirirrin@gmail.com>
2025-06-24 20:04:18 +0500

    Add auth for gelbooru

--- Imouto.BooruParser.Tests/Loaders/Fixtures/GelbooruApiLoaderFixture.cs
+++ Imouto.BooruParser.Tests/Loaders/Fixtures/GelbooruApiLoaderFixture.cs
@@ -9,9 +9,14 @@ public class GelbooruApiLoaderFixture
 {
     private IBooruApiLoader? _loader;
     private readonly bool _enableCache = true;
-    
-    private readonly IOptions<GelbooruSettings> _options 
-        = Options.Create(new GelbooruSettings { PauseBetweenRequestsInMs = 0 });
+
+    private readonly IOptions<GelbooruSettings> _options
+        = Options.Create(new GelbooruSettings
+        {
+            PauseBetweenRequestsInMs = 0,
+            UserId = 1740518,
+            ApiKey = "e975ef828d4789449b469a36fabd60afe981a4de010fa40922b4e42adbdf22d052a4ef41fc4d767a59bb04121c79b411eea020707084433bf2b05c23597c97c2"
+        });
 
     private IFlurlClientCache Factory =>
         _enableCache
